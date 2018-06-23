010d513a7ece4b6ae5c9e4cb7efa3d5ae654a486
esteele <eric@esteele.net>
2018-06-23 10:32:02 +0200

    Add wireframe for actions

---
+++ src/actions/addons/addons.js
@@ -0,0 +1,40 @@
+/**
+ * Add-ons actions.
+ * @module actions/addons/addons
+ */
+
+import {
+  GET_ADDON,
+  INSTALL_ADDON,
+  LIST_ADDONS,
+  UNINSTALL_ADDON,
+  UPGRADE_ADDON,
+} from '../../constants/ActionTypes';
+
+/**
+ * Get addon function.
+ * @function getAddon
+ * @param {string} id addon id
+ * @returns {Object} Get addon action
+ */
+export function getAddon(content) {
+  return {
+    type: GET_ADDON,
+    promise: api => api.get('/@addons', { data: content }),
+  };
+}
+
+/**
+ * List users function
+ * @function listAddons
+ * @param {string} query Query
+ * @returns {Object} List addons action
+ */
+export function listAddons(query) {
+  return {
+    type: LIST_ADDONS,
+    promise: query
+      ? api => api.get(`/@addons?query=${query}`)
+      : api => api.get('/@addons'),
+  };
+}
